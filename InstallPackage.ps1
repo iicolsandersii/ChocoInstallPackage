ad612e1a16e79470cf3c427d4b4d538a30633770
Josh Lander <joshlander@gmail.com>
2016-09-29 09:27:13 -0700

    Update InstallPackage.ps1

--- InstallPackage.ps1
+++ InstallPackage.ps1
@@ -1,4 +1,5 @@
 # Description: This script can be used to install a single, or multiple applications via choco example: firefoxesr,flashplayerplugin
+# Also checks if Chocolatey is installed, if now installs the latest Chocolatey
 
 Param(
   [string[]]$packages
@@ -18,8 +19,8 @@ try
         #install choco
         iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))    
     
-        #adds our local package source
-        &choco source add -n=chocv-sa-ap01 -s "http://chocv-sa-ap01/chocolatey" -y
+        #adds your local package source
+        #&choco source add -n=server-name -s "http://server-name/chocolatey" -y
 
         #re-readfresh environmental variables
         $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")     
