3032c646b9a000a7cdce6aca858736661cf6010f
Josh Lander <joshlander@gmail.com>
2017-02-13 07:33:37 -0800

    Update and rename InstallPackageLANDEsk.ps1 to InstallPackageLANDesk.ps1
    Simplified the script so that now, it uses upgrade instead of querying the system to find out if the app already exists.
    
    Moved the custom source add after the environmental variables refresh.
    
    Re-ordered the LANDesk messages so it's a bit clearer which app it's currently installed / completed if passing several.
    
    Changed file name to InstallPackageLANdesk.ps1.

---
+++ InstallPackageLANDesk.ps1
@@ -0,0 +1,52 @@
+#Description: This script can be used to install a single, or multiple applications via choco example: firefoxesr,flashplayerplugin
+
+Param(
+  [array[]]$packages
+)
+
+#chocolately path
+$ChocoPath = "$env:ProgramData\chocolatey\bin\choco.exe"
+
+#check if chocolatey is installed
+$ChocoInstalled = test-Path $ChocoPath
+
+try 
+{
+    #if Choco is not installed, lets install it.
+    if (!$ChocoInstalled)
+    {
+        #message back to LANDesk Core
+        &"${env:ProgramFiles(x86)}\LANDesk\LDClient\SDCLIENT.EXE" /msg="Chocolatey Installing..."
+
+        #install choco
+        iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex 
+
+        #re-readfresh environmental variables
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+        #adds our local package source
+        &choco source add -n=chocv-sa-ap01 -s "http://chocv-sa-ap01/chocolatey"
+    }
+
+        #install the applications (will upgrade existing package, otherwise will install)
+    foreach ($package in $packages)
+    {
+        #message back to LANDesk Core
+        &"${env:ProgramFiles(x86)}\LANDesk\LDClient\SDCLIENT.EXE" /msg="Installing $package..."
+
+        &choco upgrade $package -y -f
+
+        #message back to LANDesk Core
+        &"${env:ProgramFiles(x86)}\LANDesk\LDClient\SDCLIENT.EXE" /msg="$package installation success!"
+    }
+    
+    &"${env:ProgramFiles(x86)}\LANDesk\LDClient\SDCLIENT.EXE" /msg="Install Complete!"
+}
+catch
+{
+    &"${env:ProgramFiles(x86)}\LANDesk\LDClient\SDCLIENT.EXE" /msg="An error occurred: $_.Exception.message"
+
+    #if there are any errors or issues, output to a file at the path below
+    $_.Exception.message | Out-File "c:\InstallPackage.txt"
+    Write-Host $_.Exception.message
+}
